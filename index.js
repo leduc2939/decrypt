b94a40f014d21d96bb456cdc2eb7c4e220dc7ef0
ducle <leduc2939@gmail.com>
2023-11-27 03:54:42 +0700

    resolve game round

--- index.js
+++ index.js
@@ -17,41 +17,90 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
 
-io.on('connection', (socket) => {
+let game_state = {};
+let round_no = 1;
+let user_team = {};
+game_state[`round_${round_no}`] = {}; 
+game_state[`round_${round_no}`]['team_1'] = {};
+game_state[`round_${round_no}`]['team_2'] = {};
+let misconmunication = {'team_1':0, 'team_2':0};
+let interception = {'team_1':0,'team_2':0};
+let position_to_be_encoded = {};
+position_to_be_encoded['team_1'] = [1,2,3,4];
+position_to_be_encoded['team_2'] = [1,2,3,4];
 
+
+io.on('connection', (socket) => {
+  user_team[socket.id] = 1;
   console.log('a user connected');
 
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 
+
+
   socket.on('newGames', (user_id) => {
     console.log('user: ' + user_id + ' has started a new game');
     io.emit('newGame_JS');
   })
-
+  
+  // 
   socket.on('giveClues', (user_id) => {
     console.log('user: ' + user_id + ' is giving clues');
-    position_to_be_encoded = [1, 2, 3, 4];
-    left_out_position = Math.floor(Math.random()*position_to_be_encoded.length);
-    position_to_be_encoded.splice(left_out_position, 1);
-    io.emit('giveClues_JS', user_id, position_to_be_encoded, left_out_position+1);
+    var team = user_team[user_id];
+    var left_out_position = Math.floor(Math.random()*4);
+    position_to_be_encoded[`team_${team}`].splice(left_out_position,1);
+    left_out_position += 1;
+    game_state[`round_${round_no}`][`team_${team}`][`box${team}${position_to_be_encoded[`team_${team}`][0]}`] = '';
+    game_state[`round_${round_no}`][`team_${team}`][`box${team}${position_to_be_encoded[`team_${team}`][1]}`] = '';
+    game_state[`round_${round_no}`][`team_${team}`][`box${team}${position_to_be_encoded[`team_${team}`][2]}`] = '';
+    game_state[`round_${round_no}`][`team_${team}`][`box${team}${left_out_position}`] = 'null';
+    io.emit('giveClues_JS', user_id, position_to_be_encoded[`team_${team}`]);
   });
 
   // rearrange clues then give to other players
-  socket.on('submitClues', (user_id, input1_value, input2_value,input3_value) => {
-    console.log(input1_value);
-    console.log(input2_value);
-    console.log(input3_value);
+  socket.on('submitClues', (user_id, input1_value, input2_value, input3_value) => {
+    console.log(input1_value,input2_value,input3_value);
+    var team = user_team[user_id];
+    game_state[`round_${round_no}`][`team_${team}`][`box${team}${position_to_be_encoded[`team_${team}`][0]}`] = input1_value;
+    game_state[`round_${round_no}`][`team_${team}`][`box${team}${position_to_be_encoded[`team_${team}`][1]}`] = input2_value;
+    game_state[`round_${round_no}`][`team_${team}`][`box${team}${position_to_be_encoded[`team_${team}`][2]}`] = input3_value;
     // randomize position
-    position_to_be_encoded = [1, 2, 3, 4];
-    left_out_position = Math.floor(Math.random()*position_to_be_encoded.length);
-    position_to_be_encoded.splice(left_out_position, 1);
-    io.emit('rearrangeClues_JS', user_id, input2_value, input3_value, input1_value, position_to_be_encoded, left_out_position+1);
+    var mixed_position = [1, 2, 3, 4];
+    var left_out_position = Math.floor(Math.random()*mixed_position.length);
+    mixed_position.splice(left_out_position,1);
+    io.emit('rearrangeClues_JS', user_id, input2_value, input3_value, input1_value, mixed_position);
   });
   
+  socket.on('suggestAnswer', (user_id, d) => {
+    var team = user_team[user_id];
+    io.emit('rearrangeSuggest_JS', user_id, d);
+    console.log('emit rearrangeSuggest_JS');
+  });
 
+  socket.on('submitAnswer', (user_id, d) => {
+    var team = user_team[user_id];
+    console.log(d);
+    console.log(position_to_be_encoded[`team_${team}`]);
+    console.log(game_state);
 
+    if (
+      d[`box1${position_to_be_encoded[`team_${team}`][0]}`] == game_state[`round_${round_no}`][`team_${team}`][`box1${position_to_be_encoded[`team_${team}`][0]}`] &&
+      d[`box1${position_to_be_encoded[`team_${team}`][1]}`] == game_state[`round_${round_no}`][`team_${team}`][`box1${position_to_be_encoded[`team_${team}`][1]}`] &&
+      d[`box1${position_to_be_encoded[`team_${team}`][2]}`] == game_state[`round_${round_no}`][`team_${team}`][`box1${position_to_be_encoded[`team_${team}`][2]}`]
+    ) {
+      console.log('correct');
+      io.emit('pushRight', 1, user_id, game_state[`round_${round_no}`][`team_${team}`]);
+      console.log('push right signal sent');
+    } else {
+      console.log('incorrect');
+      misconmunication[`team_${team}`] +=1;
+      io.emit('pushRight', 0, user_id, game_state[`round_${round_no}`][`team_${team}`]);
+      console.log('push right signal sent');
+    }
+    console.log(misconmunication);
+  });
 
 
   socket.on('chat message', (msg) => {
--- public/script.js
+++ public/script.js
@@ -1,81 +1,5 @@
-// var columns = document.querySelectorAll('.draggable_clue_1');
-// var columns2 = document.querySelectorAll('.leftout');
-// var draggingClass = 'dragging';
-// var dragSource;
-
-// Array.prototype.forEach.call(columns, function (col) {
-//   col.addEventListener('dragstart', handleDragStart, false);
-//   col.addEventListener('dragenter', handleDragEnter, false)
-//   col.addEventListener('dragover', handleDragOver, false);
-//   col.addEventListener('dragleave', handleDragLeave, false);
-//   col.addEventListener('drop', handleDrop, false);
-//   col.addEventListener('dragend', handleDragEnd, false);
-// });
-
-// function handleDragStart (evt) {
-//   // console.log('start drag');
-//   dragSource = this;
-//   evt.target.classList.add(draggingClass);
-//   evt.dataTransfer.effectAllowed = 'move';
-//   evt.dataTransfer.setData('text/html', this.innerHTML);
-// }
-
-// function handleDragOver (evt) {
-//   // console.log('drag over');
-//   evt.dataTransfer.dropEffect = 'move';
-//   evt.preventDefault();
-// }
-
-// function handleDragEnter (evt) {
-//   // console.log('drag enter');
-//   this.classList.add('over');
-//   evt.preventDefault();
-// }
-
-// function handleDragLeave (evt) {
-//   // console.log('drag leave');
-//   this.classList.remove('over');
-// }
-
-// function handleDrop (evt) {
-//   // console.log('drag drop');
-//   evt.stopPropagation();
-  
-//   if (dragSource !== this) {
-//     dragSource.innerHTML = this.innerHTML;
-//     this.innerHTML = evt.dataTransfer.getData('text/html');
-//   }
-  
-//   evt.preventDefault();
-// }
-
-// function handleDragEnd (evt) {
-//   console.log('drag end');
-//   Array.prototype.forEach.call(columns, function (col) {
-//     ['over', 'dragging'].forEach(function (className) {
-//       col.classList.remove(className);
-//     });
-//   });
-//   evt.target.classList.remove('drag-over');
-
-// }
-
-
-
-
-// const item = document.querySelector('.item');
-
-// item.addEventListener('dragstart', dragStart);
-
-// function dragStart(e) {
-//   e.dataTransfer.setData('text/plain', e.target.id);
-//   setTimeout(() => {
-//       e.target.classList.add('hide');
-//   }, 0);
-// }
 
 var columns = document.querySelectorAll('.draggable_clue_1');
-// var columns2 = document.querySelectorAll('.leftout');
 var draggingClass = 'dragging';
 var dragSource;
 
@@ -122,15 +46,27 @@ function drop(e) {
   const id = e.dataTransfer.getData('text/plain');
   // console.log("id: " + id);
   const draggable = document.getElementById(id);
-  // console.log(draggable);
-  // console.log(draggable.parentNode);
-  if (e.target.id.startsWith('b')){
+  console.log(draggable);
+  console.log(draggable.parentNode);
+  var drag_parent = draggable.parentNode;
+  if (e.target.id.startsWith('b') && e.target.firstChild == null){
     e.target.appendChild(draggable);
+  } 
+  else if (e.target.id.startsWith('b') && e.target.firstChild) {
+    drag_parent.append(e.target.firstChild);
+    e.target.append(draggable);
+  }
+  else {
+    console.log(e.target.parentNode);
+    e.target.parentNode.append(draggable);
+    // draggable.parentNode.append(e.target);
+    drag_parent.append(e.target);
   }
   // add it to the drop target
-  // console.log(e.target.id);
+  console.log(e.target.id);
   
-  // console.log(e.target);
+  console.log(e.target);
+  console.log(e.target.firstChild);
   // display the draggable element
   draggable.classList.remove('hide');
 }
@@ -147,7 +83,7 @@ function newGame_JS() {
   $('#loser_word4 span').text('random word');
 }
 
-function giveClues_JS (position_to_be_encoded,left_out_position) {
+function giveClues_JS (position_to_be_encoded) {
   let clue_input11 = document.createElement("div");
   clue_input11.id = "input11";
   clue_input11.setAttribute('type','text');
@@ -171,15 +107,9 @@ function giveClues_JS (position_to_be_encoded,left_out_position) {
   $(`#box1${position_to_be_encoded[1]}`).prepend(clue_input12);
   $(`#box1${position_to_be_encoded[2]}`).prepend(clue_input13);
 
-  // create a transparent box for the one that's left out
-  // let clue_input14 = document.createElement("div");
-  // clue_input14.id = "leftout1";
-  // clue_input14.setAttribute('type','text');
-  // clue_input14.setAttribute('class','leftout');
-  // $(`#row1${left_out_position}`).prepend(clue_input14);
 }
 
-function rearrangeClues_JS(input2_value, input3_value, input1_value, position_to_be_encoded, left_out_position) {
+function rearrangeClues_JS(input2_value, input3_value, input1_value, position_to_be_encoded) {
   let clue1 = document.createElement("div");
   clue1.id = "input11";
   // clue1.setAttribute('type','text');
@@ -206,13 +136,6 @@ function rearrangeClues_JS(input2_value, input3_value, input1_value, position_to
   $(`#box1${position_to_be_encoded[2]}`).prepend(clue3);
   console.log('appened')
 
-  // let clue_input14 = document.createElement("div");
-  // clue_input14.id = "leftout1";
-  // // clue_input14.setAttribute('type','text');
-  // clue_input14.setAttribute('class','leftout');
-  // clue_input14.setAttribute('draggable', 'true');
-  // $(`#row1${left_out_position}`).prepend(clue_input14);
-
   columns = document.querySelectorAll('.draggable_clue_1');
   draggingClass = 'dragging';
   dragSource;
@@ -221,69 +144,90 @@ function rearrangeClues_JS(input2_value, input3_value, input1_value, position_to
       col.addEventListener('dragstart', dragStart, false);
     });
 
-  // columns2 = document.querySelectorAll('.leftout');
-  // draggingClass = 'dragging';
-  // dragSource;
-
-  // Array.prototype.forEach.call(columns2, function (col) {
-  //     col.addEventListener('dragstart', handleDragStart, false);
-  //     col.addEventListener('dragenter', handleDragEnter, false)
-  //     col.addEventListener('dragover', handleDragOver, false);
-  //     col.addEventListener('dragleave', handleDragLeave, false);
-  //     col.addEventListener('drop', handleDrop, false);
-  //     col.addEventListener('dragend', handleDragEnd, false);
-  //   });
 }
 
 
-// var columns = document.querySelectorAll('.card');
-// var draggingClass = 'dragging';
-// var dragSource;
+function rearrangeSuggest_JS (user_id, d) {
 
-// Array.prototype.forEach.call(columns, function (col) {
-//   col.addEventListener('dragstart', handleDragStart, false);
-//   col.addEventListener('dragenter', handleDragEnter, false)
-//   col.addEventListener('dragover', handleDragOver, false);
-//   col.addEventListener('dragleave', handleDragLeave, false);
-//   col.addEventListener('drop', handleDrop, false);
-//   col.addEventListener('dragend', handleDragEnd, false);
-// });
+  $('#box11').empty();
+  $('#box12').empty();
+  $('#box13').empty();
+  $('#box14').empty();
 
-// function handleDragStart (evt) {
-//   dragSource = this;
-//   evt.target.classList.add(draggingClass);
-//   evt.dataTransfer.effectAllowed = 'move';
-//   evt.dataTransfer.setData('text/html', this.innerHTML);
-// }
+  if (d['box11'] != 'null') {
+    let clue1 = document.createElement("div");
+    clue1.id = "input11";
+    clue1.setAttribute('class',"draggable_clue_1");
+    clue1.setAttribute('draggable', 'true');
+    clue1.innerHTML  = d['box11'];
+    $('#box11').prepend(clue1);
+  }
+  if (d['box12'] != 'null') {
+    let clue2 = document.createElement("div");
+    clue2.id = "input12";
+    clue2.setAttribute('class',"draggable_clue_1");
+    clue2.setAttribute('draggable', 'true');
+    clue2.innerHTML  = d['box12'];
+    $('#box12').prepend(clue2);
+  }
+  if (d['box13'] != 'null') {
+    let clue3 = document.createElement("div");
+    clue3.id = "input13";
+    clue3.setAttribute('class',"draggable_clue_1");
+    clue3.setAttribute('draggable', 'true');
+    clue3.innerHTML  = d['box13'];
+    $('#box13').prepend(clue3);
+  }
+  if (d['box14'] != 'null') {
+    let clue4 = document.createElement("div");
+    clue4.id = "input14";
+    clue4.setAttribute('class',"draggable_clue_1");
+    clue4.setAttribute('draggable', 'true');
+    clue4.innerHTML  = d['box14'];
+    $('#box14').prepend(clue4);
+  }
+ 
+  columns = document.querySelectorAll('.draggable_clue_1');
+  draggingClass = 'dragging';
+  dragSource;
+    
+  Array.prototype.forEach.call(columns, function (col) {
+      col.addEventListener('dragstart', dragStart, false);
+    });
 
-// function handleDragOver (evt) {
-//   evt.dataTransfer.dropEffect = 'move';
-//   evt.preventDefault();
-// }
+  var alert = document.createElement("div");
+  alert.setAttribute('class',"suggest_alert");
+  alert.setAttribute('id',"suggest_alert");
+  alert.innerHTML  = user_id + ' has given a suggestion';
+  console.log(alert);
+  $('#container').append(alert);
+  console.log('appended to container');
+  setTimeout(function() {
+    $(alert).fadeTo(3000, 0).slideUp(3000, function(){
+      $(this).remove();
+    });
+  });
+}
 
-// function handleDragEnter (evt) {
-//   this.classList.add('over');
-// }
-
-// function handleDragLeave (evt) {
-//   this.classList.remove('over');
-// }
-
-// function handleDrop (evt) {
-//   evt.stopPropagation();
+function pushRight (result, user_id, game_state_correct_answerd) {
+  if (result == 1){
+    console.log(user_id + " has nailed the thing");
+  } else {
+    console.log(user_id + " has failed the thing");
+  }
+  console.log(game_state_correct_answerd);
+  for (key in game_state_correct_answerd){
+    if (game_state_correct_answerd[key] != 'null') {
+      let previous_words_1 = document.createElement("div");
+      previous_words_1.setAttribute('class',"previous_words_1");
+      previous_words_1.innerHTML  = game_state_correct_answerd[key];
+      $(`#row${key.slice(-2)}`).append(previous_words_1);
+    }
+    else {
+      let previous_words_1 = document.createElement("div");
+      previous_words_1.setAttribute('class',"previous_words_1_empty");
+      $(`#row${key.slice(-2)}`).append(previous_words_1);
+    }
+  }
   
-//   if (dragSource !== this) {
-//     dragSource.innerHTML = this.innerHTML;
-//     this.innerHTML = evt.dataTransfer.getData('text/html');
-//   }
-  
-//   evt.preventDefault();
-// }
-
-// function handleDragEnd (evt) {
-//   Array.prototype.forEach.call(columns, function (col) {
-//     ['over', 'dragging'].forEach(function (className) {
-//       col.classList.remove(className);
-//     });
-//   });
-// }
+}
