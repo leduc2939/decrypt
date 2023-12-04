22aa2b780c770212307de4e72bbceddd5a3e84e4
ducle <leduc2939@gmail.com>
2023-12-04 10:56:30 +0700

    change PORT

--- index.js
+++ index.js
@@ -656,7 +656,7 @@ io.on('connection', (socket) => {
 
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
 });
 
